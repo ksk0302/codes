6e85e5f48b13e84ca90a7e7734aec86f6448c833
ksk0302 <63835048+ksk0302@users.noreply.github.com>
2023-01-19 23:07:53 +0900

    Add files via upload

---
+++ atcoder/AtCoder Beginner Contest/274/D.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <limits.h>
+#include <map>
+#include <set>
+#include <queue>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, x, y; cin >> N >> x >> y;
+    veci A(N+1), dpx(N+1), dpy(N+1);
+    for(int i=1; i<N+1; i++) cin >> A[i];
+}
