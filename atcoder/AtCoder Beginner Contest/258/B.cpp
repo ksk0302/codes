cea1fe0d6c2d09ecbb977b672c2331b8566d7b3c
ksk0302 <63835048+ksk0302@users.noreply.github.com>
2023-01-19 23:02:01 +0900

    Add files via upload

---
+++ atcoder/AtCoder Beginner Contest/258/B.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <limits.h>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+using ll = long long;
+using veci = vector<int>;
+using vecll = vector<ll>;
+using vecd = vector<double>;
+using vveci = vector<veci>;
+using vvecll = vector<vecll>;
+using vvecd = vector<vecd>;
+#define rep(i, l, r) for(int i=l; i<r; i++)
+#define rrep(i, l, r) for(int i=r-1; i>=l; i--)
+
+vector<int> dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+vector<int> dy = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+int main() {
+}
